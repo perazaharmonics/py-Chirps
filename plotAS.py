238cc7c5bffb4236fe6538f10bf4ecdfed653b0e
Enrique, MEngEE DSP <141517235+perazaharmonics@users.noreply.github.com>
2024-02-21 13:27:00 -0500

    Update and rename PlotAmbiguity.py to plotAS.py
    Changed variable names

--- plotAS.py
+++ plotAS.py
@@ -2,32 +2,30 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
-class PlotAmbiguity:
-
+class plotAS:
     def __init__(self):
-        pass  # Initialize if needed
-    
-    def plot_ambiguity_function_3d(self, ambiguity_surface, t, fc, tau_range, fd_range):
+        pass
+    def plot_AS(self, AS, t, fc, tau_range, fd_range):
         tau_grid, fd_grid = np.meshgrid(tau_range, fd_range)
         fig = plt.figure(figsize=(12, 5))
         
         ax1 = fig.add_subplot(131, projection='3d')
-        ax1.plot_surface(tau_grid, fd_grid, np.abs(ambiguity_surface), cmap='viridis')
+        ax1.plot_surface(tau_grid, fd_grid, np.abs(AS), cmap='viridis')
         ax1.set_xlabel('Delay (s)')
         ax1.set_ylabel('Doppler (Hz)')
         ax1.set_zlabel('Ambiguity function')
         ax1.set_title('Ambiguity function for fc = '+str(fc)+' Hz')
 
         ax2 = fig.add_subplot(132)
-        mag = np.abs(ambiguity_surface)
+        mag = np.abs(AS)
         ax2.plot(tau_range, 20*np.log10(mag[:, 50]), label='Doppler = '+str(fd_range[50])+' Hz')
         ax2.set_xlabel('Delay (s)')
         ax2.set_ylabel('Magnitude (dB)')
         ax2.set_title('Magnitude of ambiguity function')
         ax2.legend()
 
-        threshold = 0.05 * np.max(np.abs(ambiguity_surface))
-        phase = np.angle(ambiguity_surface * (np.abs(ambiguity_surface) > threshold))
+        threshold = 0.05 * np.max(np.abs(AS))
+        phase = np.angle(AS * (np.abs(AS) > threshold))
 
         ax3 = fig.add_subplot(133)
         ax3.plot(tau_range, phase[:, 50], label='Doppler = '+str(fd_range[50])+' Hz')
